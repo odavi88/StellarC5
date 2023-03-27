d61a9b30d0cb3461165c4d5cbec95714eb156bc4
Jacob M Sock <jacobmsock@gmail.com>
2023-03-27 15:51:04 -0400

    add rough ui for profile view

--- StellarCopy/Views/ProfileView.swift
+++ StellarCopy/Views/ProfileView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("Profile View")
+        
+        VStack {
+            Text("Welcome 'Username'")
+            Image(systemName: "circle.circle").font(.largeTitle)
+            Image(systemName: "moon.fill")
+            Text("Horoscope description")
+        }
+        
+      
     }
 }
 
