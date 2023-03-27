c6702770df7dc1c8a07e43301dcaadad12b474ec
Jacob M Sock <jacobmsock@gmail.com>
2023-03-27 16:48:30 -0400

    add Moon Model

---
+++ StellarCopy/Models/Moon.swift
@@ -0,0 +1,14 @@
+//
+//  Moon.swift
+//  StellarCopy
+//
+//  Created by Jacob Sock on 3/27/23.
+//
+
+import Foundation
+
+
+struct Moon : Codable {
+    var id = UUID()
+    var moonrise: String
+}
