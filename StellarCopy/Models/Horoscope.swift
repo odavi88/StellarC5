89c9ebb2880b2e36c73d929e7ec81287b61364e4
India Doria <60370426+clamquarter@users.noreply.github.com>
2023-03-27 16:35:36 -0400

    Add Horoscope Model/Struct.

---
+++ StellarCopy/Models/Horoscope.swift
@@ -0,0 +1,33 @@
+//
+//  Horoscope.swift
+//  Countries
+//
+//  Created by India Doria on 3/27/23.
+//
+
+import Foundation
+
+
+struct Prediction : Codable{
+    var id = UUID()
+
+    var personal_life : String
+    
+    var profession : String
+    
+    var travel : String
+    var emotions : String
+    
+}
+
+
+struct Horoscope: Codable {
+    var id = UUID()
+    var prediction_date: String
+    
+    var status : Bool
+    //will do an enum later
+    var sun_sign: String
+//    var imageName: String
+    var prediction: Prediction
+}
