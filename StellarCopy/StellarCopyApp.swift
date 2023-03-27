10608f66706a21f527d5edd31429ee545e716418
Jacob M Sock <jacobmsock@gmail.com>
2023-03-27 14:04:36 -0400

    Initial Commit

---
+++ StellarCopy/StellarCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StellarCopyApp.swift
+//  StellarCopy
+//
+//  Created by Jacob Sock on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct StellarCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
